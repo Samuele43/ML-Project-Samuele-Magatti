bd1031dad108650498af9f9c4a0918eda97210a3
Samuele Magatti <samuele@Air-di-Samuele.homenet.telecomitalia.it>
2025-10-04 23:52:35 +0200

    update of first part of code

--- Samuele.Magatti.ML.Project.py
+++ Samuele.Magatti.ML.Project.py
@@ -1,1 +1,60 @@
 #ML project
+
+# Check if you have installed data correctly
+import os
+
+if not os.path.exists ("data")
+   raise FileNotFoundError("put the data in a folder called 'data/' as written in the README file")
+
+#Check if the data are images
+
+import glob
+
+images = glob.glob("data/*.jpg") + glob.glob("data/*.png")
+if len(images) == 0:
+    raise FileNotFoundError("No images found in data/ (expected .jpg or .png files)")
+
+
+
+#Data exploration 
+
+
+
+
+# Preprocessing
+
+
+
+## image resizing
+
+## normalization
+
+## data augmentation
+
+## split data into train and test set
+
+
+
+
+required_files = ["data/train.csv", "data/test.csv"]
+
+for f in required_files:
+    if not os.path.exists(f):
+        raise FileNotFoundError(f"Missing {f}. Put the data in a folder called 'data/' as written in the README file")
+
+
+
+#build of first CNN
+
+
+
+
+
+
+
+
+
+
+
+
+
