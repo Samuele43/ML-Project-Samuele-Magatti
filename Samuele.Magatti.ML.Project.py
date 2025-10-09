d11109ca379cf5c366b469cdca2e8b00c168eb7c
Samuele Magatti <samuele@Air-di-Samuele.station>
2025-10-09 22:38:00 +0200

    descrizione della modifica

--- Samuele.Magatti.ML.Project.py
+++ Samuele.Magatti.ML.Project.py
@@ -7,6 +7,12 @@ import os
 if not os.path.exists ("data"):
    raise FileNotFoundError("put the data in a folder called 'data/' as written in the README file")
 
+#Data exploration 
+## From the readme I know that:
+# All images are RGB images of 300 pixels wide by 200 pixels high in .png format. 
+# the images are separated in three sub-folders named 'rock', 'paper' and 'scissors' according to their respective class.
+
+
 #Check if the data are images and count the total number of images in all the files in data folder
 
 import glob
@@ -19,23 +25,90 @@ else:
     print(f"Found {len(images)} PNG files.")
     
 
-#Data exploration 
-## From the readme I know that:
-# All images are RGB images of 300 pixels wide by 200 pixels high in .png format. 
-# the images are separated in three sub-folders named 'rock', 'paper' and 'scissors' according to their respective class.
+# find and eliminate corrupted files
+
+from PIL import Image
+import os
+
+base_dir = "data"
+
+classes = [d for d in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, d))]
+
+corrupted = []  
+valid_extensions = {".png"}
+
+for cls in classes:
+    class_dir = os.path.join(base_dir, cls)
+    for f in os.listdir(class_dir):
+        img_path = os.path.join(class_dir, f)
+
+        # check if it's an image
+        if not os.path.isfile(img_path):
+            continue
+        if not any(f.lower().endswith(ext) for ext in valid_extensions):
+            continue
+
+        try:
+            with Image.open(img_path) as img:
+                img.verify() 
+        except Exception as e:
+            print(f" corruoted image: {img_path} ({e})")
+            corrupted.append(img_path)
+
+print(f"\nTotal corrupted images: {len(corrupted)}")
+
+# eliminate corrupted images
+
+for img_path in corrupted:
+    os.remove(img_path)  
+
+# find and eliminate duplicate images
+
+import os
+import hashlib
+
+# compute the hash MD5 of a file
+
+def hash_file(file_path):
+    
+    hasher = hashlib.md5()
+    with open(file_path, "rb") as f:
+        buf = f.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
+
+base_dir = "data"
+duplicates = {}
+for root, dirs, files in os.walk(base_dir):
+    if "rps-cv-images" in root:
+        continue
+    for f in files:
+        if f.lower().endswith((".png")):
+            path = os.path.join(root, f)
+            h = hash_file(path)
+            if h in duplicates:
+                print(f"Duplicate found: {path} is identical to {duplicates[h]}")
+            else:
+                duplicates[h] = path
+
+
+# eliminate duplicates
+
+for path in duplicates:
+    os.remove(path)
+
 
 #number of images for each class
 
 import os
 
-base_dir = "data"
 
 # Take ino account only the main folder data 
 
 counts = {
     cls: len([
         f for f in os.listdir(os.path.join(base_dir, cls))
-        if f.lower().endswith(('.jpg', '.png'))
+        if f.lower().endswith(('.png'))
     ])
     for cls in os.listdir(base_dir)
     if os.path.isdir(os.path.join(base_dir, cls)) and cls != "rps-cv-images"
@@ -74,20 +147,13 @@ import seaborn as sns
 
 #check the size of each image for each class
 
-base_dir = "data" 
-
-#list of classes
-
-classes = [d for d in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, d))]
-
-
 # create a dictionary list with name, width and height
 data = []
 
 for cls in classes:
     class_dir = os.path.join(base_dir, cls)
     for filename in os.listdir(class_dir):
-        if filename.lower().endswith(('.jpg', '.png')):
+        if filename.lower().endswith(('.png')):
             path = os.path.join(class_dir, filename)
             with Image.open(path) as img:
                 width, height = img.size
@@ -132,8 +198,31 @@ plt.xlabel("Aspect ratio (width / height)")
 plt.ylabel("Number of images")
 plt.show()
 
+#check all the images are rgb
 
-#resize (?)
+total = 0
+non_rgb = 0
+valid_extensions = { ".png" }
+
+for cls in classes:
+    class_dir = os.path.join(base_dir, cls)
+    for img_name in os.listdir(class_dir):
+        img_path = os.path.join(class_dir, img_name)
+        if not os.path.isfile(img_path):
+            continue
+        if not any(img_name.lower().endswith(ext) for ext in valid_extensions):
+            continue
+        try:
+            with Image.open(img_path) as img:
+                total += 1
+                if img.mode != "RGB":
+                    non_rgb += 1
+                    print(f"{img_path} is in {img.mode}")
+        except Exception as e:
+            print(f"error in file {img_path}")
+
+print(f"\n total images controlled: {total}")
+print(f" non RGB images: {non_rgb}")
 
 
 # mean for the rgb values
@@ -142,32 +231,64 @@ import numpy as np
 r_means, g_means, b_means = [], [], []
 
 for cls in classes:
-    for f in os.listdir(os.path.join(base_dir, cls))[:20]:
+    class_dir = os.path.join(base_dir, cls)
+    for f in os.listdir(class_dir):
+        img_path = os.path.join(class_dir, f)
+        if not os.path.isfile(img_path):
+            continue
+        if not any(f.lower().endswith(ext) for ext in valid_extensions):
+            continue
         img = np.array(Image.open(os.path.join(base_dir, cls, f))) / 255.0
         r_means.append(img[...,0].mean())
         g_means.append(img[...,1].mean())
         b_means.append(img[...,2].mean())
 
-plt.hist(r_means, bins=30, alpha=0.5, label='R')
-plt.hist(g_means, bins=30, alpha=0.5, label='G')
-plt.hist(b_means, bins=30, alpha=0.5, label='B')
-plt.legend(); plt.show()
+#histogram
+
+plt.hist(r_means, bins=30, alpha=0.5, color='r', label='R')
+plt.hist(g_means, bins=30, alpha=0.5, color='g', label='G')
+plt.hist(b_means, bins=30, alpha=0.5, color='b', label='B')
+plt.legend() 
+plt.title("distribution of average values of rgb")
+plt.xlabel("average value")
+plt.ylabel("frequency")
+plt.show()
+
+#satndard deviation of rgb values
+
+print("standard deviation R:", np.std(r_means))
+print("standard deviation G:", np.std(g_means))
+print("standard deviation B:", np.std(b_means))
 
 
 # Preprocessing
 
+import torch
+from torch.utils.data import DataLoader, random_split
+from torchvision import datasets, transforms
 
 
-## image resizing
+## image resizing, normalization and data augmentation
 
-## normalization
+# Resize tutte le immagini a 200x300, conversione in tensor, normalizzazione RGB
+train_transform = transforms.Compose([
+    transforms.Resize((200, 300)),
+    transforms.RandomHorizontalFlip(),     # augmentation: flip casuale
+    transforms.RandomRotation(20),         # augmentation: rotazioni casuali ±20°
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5])
+])
 
-# (?) calcola prima i valori della media e della deviazione standard
-transforms.Normalize(mean=[?, ?, ?],
-                     std=[?, ?, ?])
+val_transform = transforms.Compose([
+    transforms.Resize((200, 300)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5])
+])
 
 
-## data augmentation (?)
+
+
 
 
 from torchvision import transforms
@@ -183,6 +304,15 @@ train_transform = transforms.Compose([
 
 
 ## split data into train and test set (?)
+
+#1
+
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+
+#2
 
 from sklearn.model_selection import train_test_split
 
